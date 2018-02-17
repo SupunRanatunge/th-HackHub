4a9c8a1a10bafddf4dc1826ade29f1a42ad8d5e4
SupunRanatunge <supunr.15@cse.mrt.ac.lk>
2018-02-17 20:47:55 +0530

    Before the database is connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  selector: 'app-app',
+  template: `<app-navbar></app-navbar>,
+  <router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent  { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent} from './components/navbar/navbar.component';
+import {CarouselComponent} from './components/carousel/carousel.component';
+import {routing} from './app-routing';
+import {HomeComponent} from './pages/home/home.component';
+import {HackathonComponent} from './pages/hackathon/hackathon.component';
+import {CreateHackathonComponent} from './pages/createHackathon/createHackathon.component';
+import {UpdateHackathonComponent} from './pages/updateHackathon/updateHackathon.component';
+import {DeleteHackathonComponent} from './pages/deleteHackathon/deleteHackathon.component';
+import {LoginComponent} from './pages/login/login.component';
+import {SignupComponent} from './pages/signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, routing ],
+  declarations: [ AppComponent, NavbarComponent, CarouselComponent, HomeComponent, HackathonComponent, CreateHackathonComponent,
+    UpdateHackathonComponent, DeleteHackathonComponent, LoginComponent, SignupComponent  ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
