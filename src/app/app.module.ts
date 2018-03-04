b4457d9bd6b51983cf26aaa8c92cfa235d8992fa
SupunRanatunge <supunr.15@cse.mrt.ac.lk>
2018-03-04 12:17:12 +0530

    Before the database is connected stage 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent} from './components/navbar/navbar.component';
 import {CarouselComponent} from './components/carousel/carousel.component';
-import {routing} from './app-routing';
+import {routing, appRoutingProviders} from './app-routing';
 import {HomeComponent} from './pages/home/home.component';
 import {HackathonComponent} from './pages/hackathon/hackathon.component';
 import {CreateHackathonComponent} from './pages/createHackathon/createHackathon.component';
@@ -11,11 +11,16 @@ import {UpdateHackathonComponent} from './pages/updateHackathon/updateHackathon.
 import {DeleteHackathonComponent} from './pages/deleteHackathon/deleteHackathon.component';
 import {LoginComponent} from './pages/login/login.component';
 import {SignupComponent} from './pages/signup/signup.component';
+import {ProfileComponent} from './pages/profile/profile.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, routing ],
   declarations: [ AppComponent, NavbarComponent, CarouselComponent, HomeComponent, HackathonComponent, CreateHackathonComponent,
-    UpdateHackathonComponent, DeleteHackathonComponent, LoginComponent, SignupComponent  ],
-  bootstrap:    [ AppComponent ]
+    UpdateHackathonComponent, DeleteHackathonComponent, LoginComponent, SignupComponent, ProfileComponent  ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    appRoutingProviders,
+  ]
 })
 export class AppModule { }
