672e7094a2e5d92ca075f448c798bd478dcaaa52
SupunRanatunge <supunr.15@cse.mrt.ac.lk>
2018-03-06 23:31:02 +0530

    connecting to database

---
+++ Back-HackHub/HackHub-Client/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-carousel',
+  templateUrl: `carousel.component.html`,
+})
+export class CarouselComponent  {}
